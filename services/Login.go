cf74bb06a85b9d08f98aaf05150f08ca259d0f5e
Fu zhicheng <fuzhicheng@guanxingtuan.top>
2023-10-26 16:28:36 +0800

    feat.完善login

--- services/Login.go
+++ services/Login.go
@@ -5,6 +5,10 @@ import (
 )
 
 func Login(un string, pw string) (bool, error) {
+	//为空直接返回
+	if un==""||pw==""{
+		return false,nil
+	}
 	//比较密码
 	if p, err := dao.Login(un); err != nil {
 		return false, err
